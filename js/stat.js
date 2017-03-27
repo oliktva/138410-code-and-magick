9f16bcfb4b48d506c6026cb5c718016302691c65
oliktva <oliktva@gmail.com>
2017-03-27 23:35:20 +0300

    rename functions

--- js/stat.js
+++ js/stat.js
@@ -8,7 +8,7 @@ var getMax = function (arrayOfNumbers) {
   return max;
 };
 
-var createCloud = function (ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   var width = 420;
   var height = 270;
   ctx.fillStyle = color;
@@ -26,34 +26,34 @@ var createCloud = function (ctx, x, y, color) {
   ctx.stroke();
 };
 
-var createText = function (ctx, x, y, text) {
+var renderText = function (ctx, x, y, text) {
   ctx.fillStyle = 'rgb(0, 0, 0)';
   ctx.font = '16px PT Mono';
   ctx.fillText(text, x, y);
 };
 
-var createRating = function (ctx, name, time, maxTime, index) {
+var renderRating = function (ctx, name, time, maxTime, index) {
   var ratio = (time / maxTime).toFixed(2);
   var xCoordinate = 130 + 90 * index;
   var opacity = (Math.random() + 0.1).toFixed(1);
   var color = name === 'Вы' ? 'rgb(255, 0, 0)' : 'rgba(2, 14, 134, ' + opacity + ')';
 
-  createText(ctx, xCoordinate, 265, name);
-  createText(ctx, xCoordinate, 87 + 150 * (1 - ratio), time);
+  renderText(ctx, xCoordinate, 265, name);
+  renderText(ctx, xCoordinate, 87 + 150 * (1 - ratio), time);
 
   ctx.fillStyle = color;
   ctx.fillRect(xCoordinate, 95 + 150 * (1 - ratio), 40, 150 * ratio);
 };
 
 window.renderStatistics = function (ctx, names, times) {
-  createCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
-  createCloud(ctx, 100, 10, 'rgb(255, 255, 255)');
+  renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, 100, 10, 'rgb(255, 255, 255)');
 
-  createText(ctx, 120, 40, 'Ура, вы победили!');
-  createText(ctx, 120, 64, 'Список результатов:');
+  renderText(ctx, 120, 40, 'Ура, вы победили!');
+  renderText(ctx, 120, 64, 'Список результатов:');
 
   var maxTime = Math.round(getMax(times));
   for (var i = 0; i < names.length; i++) {
-    createRating(ctx, names[i], Math.round(times[i]), maxTime, i);
+    renderRating(ctx, names[i], Math.round(times[i]), maxTime, i);
   }
 };
